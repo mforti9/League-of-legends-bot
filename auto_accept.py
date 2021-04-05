c07e997ce5b84e866380cdf57fed0dedc2fdb8e9
mforti9 <59103522+mforti9@users.noreply.github.com>
2021-04-05 14:40:45 -0700

    commit

--- auto_accept.py
+++ auto_accept.py
@@ -15,18 +15,18 @@ def telegram_bot_sendtext(bot_message):
 
     return response.json()
 
-""" def champ_select():
+def champ_select():
     while True:
         champSelect = pyautogui.locateCenterOnScreen('champSelect.png', confidence=0.7)
         if champSelect:
             telegram_bot_sendtext(f'In Champ Select! {datetime.now()}') 
-            print("match_found")
+            print("Match Found")
             break
         else:
             pass
             time.sleep(3)
             print(datetime.now())
- """
+ 
 
 while True:
     # locate trigger on screen
@@ -45,13 +45,13 @@ while True:
 
         telegram_bot_sendtext(f'Game is ready! {datetime.now()}')
 
-        #champ_select()
+        champ_select()
 
         time.sleep(5)
 
         
-    elif champSelect:
-        telegram_bot_sendtext(f'In Champ Select! {datetime.now()}') 
+    # elif champSelect:
+    #     telegram_bot_sendtext(f'In Champ Select! {datetime.now()}') 
         
     elif loading:
         telegram_bot_sendtext(f'Game Loading! {datetime.now()}')
@@ -62,4 +62,4 @@ while True:
         time.sleep(3)
         print(datetime.now())
  
-# champ_select()
+
